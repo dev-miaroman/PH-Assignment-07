ad0a6f4c4bcb1ee7a6e8ab62548e53aa9d3d540a
dev-miaroman <arif.romanmia132@gmail.com>
2026-04-17 08:50:41 +0600

    adding by search option

---
+++ src/Components/TimeLine/Searching.jsx
@@ -0,0 +1,21 @@
+"use client";
+import { DataContext } from "@/context/DataContextProvider";
+import { useContext } from "react";
+import { FiSearch } from "react-icons/fi";
+
+const Searching = () => {
+  const { manageSearch } = useContext(DataContext);
+  return (
+    <div className="relative">
+      <FiSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-zinc-400 z-10 pointer-events-none" />
+      <input
+        type="search"
+        placeholder="Search by friend or interaction type"
+        className="input input-bordered w-full bg-zinc-50 border-zinc-200 pl-9 focus:outline-none focus:border-[#244d3f]-300 hover:bg-green-50 hover:border-[#244d3f]-200 transition-colors"
+        onChange={manageSearch}
+      />
+    </div>
+  );
+};
+
+export default Searching;
